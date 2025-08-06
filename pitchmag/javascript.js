3fb28abd58aade9d5a2fd147c46c99681fd17ac3
BingoMa254 <jafffeee12@gmail.com>
2025-08-06 05:03:33 +0300

    2124_vertex

---
+++ pitchmag/javascript.js
@@ -0,0 +1,20 @@
+/* 
+<?php
+require(dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-config.php');
+header("content-type: application/x-javascript");
+?> 
+*/
+
+var clear = 'assets/images/clear.gif';
+
+var imgLoading = 'assets/images/lightbox/loading.gif';
+var imgClose = 'assets/images/lightbox/closelabel.gif';
+
+var videoWidth = 300;
+var videoHeight = 225;
+var youtubeID = 'FpAgUMUlhw4';
+
+var searchDefault = 'Search this website...';
+var searchColor = '#000000';
+
+var adsenseID = '8716859555418928';
